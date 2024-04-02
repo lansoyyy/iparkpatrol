2f7676c6946787711af6cef0c9be70d5cfaae0fc
Lans12345 <olanalans12345@gmail.com>
2024-04-02 08:30:50 +0800

    added auth

--- lib/screens/auth/login_page.dart
+++ lib/screens/auth/login_page.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:iparkpatrol_mobile/screens/home_screen.dart';
 import 'package:iparkpatrol_mobile/utlis/colors.dart';
 import 'package:iparkpatrol_mobile/widgets/button_widget.dart';
 import 'package:iparkpatrol_mobile/widgets/text_widget.dart';
 import 'package:iparkpatrol_mobile/widgets/textfield_widget.dart';
+import 'package:iparkpatrol_mobile/widgets/toast_widget.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -37,7 +39,7 @@ class _LoginPageState extends State<LoginPage> {
               ),
               TextFieldWidget(
                 borderColor: primary,
-                label: 'ID Number',
+                label: 'Email',
                 controller: emailController,
               ),
               const SizedBox(
@@ -59,8 +61,7 @@ class _LoginPageState extends State<LoginPage> {
                 width: 175,
                 label: 'Login',
                 onPressed: () {
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (context) => const HomeScreen()));
+                  login(context);
                 },
               ),
               const SizedBox(
@@ -72,10 +73,37 @@ class _LoginPageState extends State<LoginPage> {
                 fontSize: 14,
                 color: primary,
               ),
+              const SizedBox(
+                height: 20,
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  login(context) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+      showToast('Logged in succesfully!');
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomeScreen()));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        showToast("No user found with that email.");
+      } else if (e.code == 'wrong-password') {
+        showToast("Wrong password provided for that user.");
+      } else if (e.code == 'invalid-email') {
+        showToast("Invalid email provided.");
+      } else if (e.code == 'user-disabled') {
+        showToast("User account has been disabled.");
+      } else {
+        showToast("An error occurred: ${e.message}");
+      }
+    } on Exception catch (e) {
+      showToast("An error occurred: $e");
+    }
   }
 }
