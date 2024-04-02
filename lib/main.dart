233fa400a8ca1d5ea1b2d306a64f23a5f41b0891
Lans12345 <olanalans12345@gmail.com>
2024-04-02 08:29:36 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:iparkpatrol_mobile/firebase_options.dart';
 import 'package:iparkpatrol_mobile/screens/auth/login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'iparkpatrol',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
