6833be46b19918f6753970cc2666dfb1f8198bb0
Lans12345 <olanalans12345@gmail.com>
2024-05-17 09:12:12 +0800

    added revisions

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:iparkpatrol_mobile/firebase_options.dart';
 import 'package:iparkpatrol_mobile/screens/auth/login_page.dart';
-import 'package:iparkpatrol_mobile/screens/pages/add_ticket_page.dart';
-import 'package:iparkpatrol_mobile/screens/pages/input_license_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,10 +17,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AddTicketPage(
-        license: '123',
-      ),
+    return const MaterialApp(
+      home: LoginPage(),
     );
   }
 }
